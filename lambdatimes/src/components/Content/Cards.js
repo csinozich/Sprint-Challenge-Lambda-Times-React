c356b60e4c3c43c611ed237d92e2435a327fa997
Claire Sinozich <claire.sinozich@gmail.com>
2019-03-23 11:23:00 -0600

    stretch: way more styled components

--- lambdatimes/src/components/Content/Cards.js
+++ lambdatimes/src/components/Content/Cards.js
@@ -1,14 +1,30 @@
 import React from 'react';
 import Card from './Card';
 import PropTypes from 'prop-types'
+import styled from 'styled-components';
+
+const CardContainer = styled.div`
+  display: flex;
+  justify-content: space-evenly;
+  align-items: none;
+  flex-direction: row;
+  width: 100%;
+  margin-top: 16px;
+  flex-wrap: wrap;
+
+  @media (min-width: 1200px) {
+    .cards-container {
+      width: 1200px;
+    }
+`;
 
 const Cards = props => {
   return (
-    <div className="cards-container">
+    <CardContainer>
       {props.cards.map((card, index) => <Card card={card} key={index}/>)}
       {/* Using the cards prop, map over the list creating a
           new Card component for each passing the card as the only prop*/}
-    </div>
+    </CardContainer>
   )
 }
 
--- lambdatimes/src/components/Content/Tabs.js
+++ lambdatimes/src/components/Content/Tabs.js
@@ -1,15 +1,45 @@
 import React from 'react';
 import Tab from './Tab';
 import PropTypes from 'prop-types'
+import styled from 'styled-components';
+
+// const TabsWrapper = styled.div`
+//   border-bottom: 1px solid lightgrey;
+//   display: flex;
+//   justify-content: center;
+//   align-items: center;
+//   flex-direction: row;
+//   width: 100%;
+//   height: 47px;
+//   background-color: #fff;
+//
+//   @media (min-width: 1280px) {
+//     .tabs {
+//       width: 1280px;
+//     }
+// `;
+//
+// const Topics = styled.div`
+//   display: flex;
+//   justify-content: space-between;
+//   align-items: center;
+//   flex-direction: row;
+// `;
+
+const Title = styled.div`
+  font-size: 12px;
+  letter-spacing: 1px;
+  font-weight: bold;
+`
 
 const Tabs = props => {
   return (
-    <div className="tabs">
-      <div className="topics">
-        <span className="title">TRENDING TOPICS:</span>
+    <div class='tabs'>
+      <div class='topics'>
+        <Title>TRENDING TOPICS:</Title>
         {props.tabs.map(tab =>
           <Tab
-          selectTabHandler={props.changeSelected}
+          selectTabHandler={props.selectTabHandler}
           selectedTab={props.selected}
           tab={tab}
           key={tab}
