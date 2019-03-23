1a181c48cb2feef2bb8d3126be16acd4ec04296e
Claire Sinozich <claire.sinozich@gmail.com>
2019-03-23 10:27:42 -0600

    card/cards/tab/tabs work

--- lambdatimes/src/components/Content/Cards.js
+++ lambdatimes/src/components/Content/Cards.js
@@ -1,12 +1,23 @@
 import React, { Component } from 'react';
 import Card from './Card';
+import PropTypes from 'prop-types'
 
 const Cards = props => {
   return (
     <div className="cards-container">
-      {/* Using the cards prop, map over the list creating a 
+      {props.cards.map(card => <Card card={props.card}/>)}
+      {/* Using the cards prop, map over the list creating a
           new Card component for each passing the card as the only prop*/}
     </div>
+  )
+}
+
+Cards.propTypes = {
+  cards: PropTypes.arrayOf(
+    PropTypes.shape({
+      headline: PropTypes.string.isRequired,
+      tab: PropTypes.oneOf(['all', 'javascript', 'technology', 'node', 'React.js', 'bootstrap'])
+    })
   )
 }
 
